3e1bec252fa2e515d11c2c5ed2893a322c44c000
takeshiisan <takeshisan29@gmail.com>
2025-05-20 11:28:40 +0800

    build

--- TextureLoader.cpp
+++ TextureLoader.cpp
@@ -16,7 +16,7 @@ SDL_Texture* TextureLoader::loadTexture(SDL_Renderer* renderer, std::string file
         }
         else {
             //Setup the relative filepath to the images folder using the input filename.
-            std::string filepath = "/Data/Images/" + filename;
+            std::string filepath = "./Data/Images/" + filename;
 
             //Try to create a surface using the filepath.
             SDL_Surface* surfaceTemp = SDL_LoadBMP(filepath.c_str());
